dc90062b578bcd5b1a97df0a7fce6d6d5bcd4e0f
Lihis <lihis@lihis.net>
2014-10-26 15:42:32 +0200

    CHANGE: Product names

--- u8800.mk
+++ u8800.mk
@@ -7,6 +7,5 @@ $(call inherit-product, device/huawei/u8800/device_u8800.mk)
 # Device identifier.
 PRODUCT_DEVICE := u8800
 PRODUCT_NAME := u8800
-PRODUCT_BRAND := ProjectHuawei
-PRODUCT_MODEL := Huawei U8800
+PRODUCT_MODEL := U8800
 PRODUCT_MANUFACTURER := Huawei
